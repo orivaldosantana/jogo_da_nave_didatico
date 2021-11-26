e8e6dd0e3949285a0c6aefe4ecd251c20fbeb681
Orivaldo Santana Jr <orivaldo@gmail.com>
2021-11-26 12:17:21 -0300

    mudança de telas

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# jogo_da_nava_didatico
-Espaço M
+# Jogo da Nave Didático
+Nome: Espaço M
+
+## Vídeos 
+* [Código para desenhar, clicar e realizar animação de três botões para o menu do jogo](https://youtu.be/Pk124w9q7jQ)  
+* [Criando uma função para verificar se o mouse está sobre um região quadrada](https://youtu.be/HKlq2I-F-OE)
+* [Organizando o código para mudar entre telas do jogo com o clique do mouse](https://youtu.be/PYTU8k0niHk)
+
+
+## Outros jogos 
+* https://github.com/orivaldosantana/jogo_da_memoria_didatico
+* https://github.com/orivaldosantana/jogo_sequencia_matematica
--- sketch.js
+++ sketch.js
@@ -5,6 +5,9 @@ var yBtn3Menu = 320;
 var larguraBtnMenu = 200; 
 var alturaBtnMenu = 50; 
 var bordaBtnMenu = 10; 
+var tela = 0;
+var larguraBordaMenu = 6; 
+var corBtnMenu = 'rgb(0,0,200)';
 
 function estaSobreBtn(yBtnMenu){
   return  mouseX > xBtnMenu && mouseX < xBtnMenu + larguraBtnMenu && mouseY > yBtnMenu && mouseY < yBtnMenu + alturaBtnMenu;
@@ -15,38 +18,70 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  strokeWeight(1);
-  fill(255);
-  if ( estaSobreBtn(yBtn1Menu) ){
-    strokeWeight(3);
-  }
-  rect(xBtnMenu,yBtn1Menu,larguraBtnMenu,alturaBtnMenu,bordaBtnMenu);
+  if (tela == 0 ){ 
+    background(0);
+    textSize(30);
+    fill(255); 
+    noStroke(); 
+    text("Espaço M", 120, 100);
+    stroke(250,200,250);
+    strokeWeight(0);
+    fill(corBtnMenu);
+    if ( estaSobreBtn(yBtn1Menu) ){
+      strokeWeight(larguraBordaMenu);
+    }
+    rect(xBtnMenu,yBtn1Menu,larguraBtnMenu,alturaBtnMenu,bordaBtnMenu);
 
-  strokeWeight(1);
-  fill(200,250,200);
-  if ( estaSobreBtn(yBtn2Menu) ){
-    strokeWeight(3);
-  }
-  rect(xBtnMenu,yBtn2Menu,larguraBtnMenu,alturaBtnMenu,bordaBtnMenu);
+    strokeWeight(0);
+    fill(corBtnMenu);
+    if ( estaSobreBtn(yBtn2Menu) ){
+      strokeWeight(larguraBordaMenu);
+    }
+    rect(xBtnMenu,yBtn2Menu,larguraBtnMenu,alturaBtnMenu,bordaBtnMenu);
 
-  strokeWeight(1);
-  fill('#FF00FF');
-  if ( estaSobreBtn(yBtn3Menu) ){
-    strokeWeight(3);
+    strokeWeight(0);
+    fill(corBtnMenu);
+    if ( estaSobreBtn(yBtn3Menu) ){
+      strokeWeight(larguraBordaMenu);
+    }
+    rect(xBtnMenu,yBtn3Menu,larguraBtnMenu,alturaBtnMenu,bordaBtnMenu);
   }
-  rect(xBtnMenu,yBtn3Menu,larguraBtnMenu,alturaBtnMenu,bordaBtnMenu);
+  if (tela == 1){
+    background(0);
+    textSize(30);
+    fill(255); 
+    noStroke(); 
+    text("Jogo em ação", 70, 100);
+  }
+  if (tela == 2){
+    background(0);
+    textSize(30);
+    fill(255); 
+    noStroke(); 
+    text("Instruções", 70, 100);
+  }
+  if (tela == 3){
+    background(0);
+    textSize(30);
+    fill(255); 
+    noStroke(); 
+    text("Créditos", 70, 100);
+  }
+
 }
 
 
 function mouseClicked() {
   if ( estaSobreBtn(yBtn1Menu) ){
-    console.log("clicou no botão branco!")
+    console.log("clicou no botão 1!")
+    tela = 1; 
   }
   if (estaSobreBtn(yBtn2Menu) ){
-    console.log("clicou no botão verde!")
+    console.log("clicou no botão 2!");
+    tela = 2; 
   }
   if ( estaSobreBtn(yBtn3Menu) ){
-    console.log("clicou no botão rosa!")
+    console.log("clicou no botão rosa!");
+    tela = 3; 
   }
 }
